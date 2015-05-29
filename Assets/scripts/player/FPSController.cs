673b70bb463f8024f49acd7ddc620c515099e26c
SpenSer Bray <matthewqa@gmail.com>
2015-05-28 17:50:27 -0700

    Simple movement done.

--- Assets/scripts/player/FPSController.cs
+++ Assets/scripts/player/FPSController.cs
@@ -7,6 +7,7 @@ public class FPSController : MonoBehaviour
 {
 	// System Members
 	public Camera Viewer;
+	public bool ComponentMultiplication = false;
 
 	// Movement Members
 	// TODO: Test and create defaults appropriate for normal circumstances.
@@ -29,5 +30,40 @@ public class FPSController : MonoBehaviour
 		// If no camera is explicitely assigned, attempt to find one
 		if(!Viewer)
 			Viewer = GetComponentInChildren<Camera>();
+	}
+
+	private void FixedUpdate()
+	{
+		// TODO: Ground Check
+		m_IsGrounded = true;
+
+		// Get direction vector based on input.
+		Vector2 input = new Vector2(Input.GetAxis("Horizontal"),
+		                            Input.GetAxis("Vertical"));
+
+		// Calculate new movement direction/speed.
+		if((Mathf.Abs(input.x) > float.Epsilon || Mathf.Abs(input.y) > float.Epsilon) && m_IsGrounded) {
+			Vector3 movement = Vector3.ProjectOnPlane(Viewer.transform.forward*input.y + Viewer.transform.right*input.x, new Vector3(0f, 1f, 0f)).normalized;
+
+			// Add speed to movement direction.
+			if(ComponentMultiplication) {
+				movement.x *= MovementSpeed;
+				movement.y *= MovementSpeed;
+				movement.z *= MovementSpeed;
+			} else {
+				movement *= MovementSpeed;
+			}
+
+			if(m_Rigidbody.velocity.sqrMagnitude < (MovementSpeed*MovementSpeed))
+				m_Rigidbody.AddForce(movement, ForceMode.Impulse);
+		}
+
+		// Check if grounded.
+		// TODO: Add air/jump code.
+		if(m_IsGrounded) {
+			m_Rigidbody.drag = 5f; // FIXME: Change to use Rigidbody's original drag, allowing editor customization.
+		} else {
+			m_Rigidbody.drag = 0f;
+		}
 	}
 }
